7f474574aaf27f95fc25a4d77e6e9369df8c8ce1
lukas0173 <vuwin24680@gmail.com>
2025-10-18 15:22:03 +0700

    [Mobile] Added the color palette, keeps the old font config from Expo initialization

--- mobile/constants/theme.ts
+++ mobile/constants/theme.ts
@@ -1,53 +1,76 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
+import { Platform } from "react-native";
 
-import { Platform } from 'react-native';
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
+// This is drived directly from the Eldritch theme
+// https://github.com/eldritch-theme
 export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+  greyDarker: {
+    name: "Sunken Depths Grey",
+    hex: "#212337",
   },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
+  grey: {
+    name: "Shallow Depths Grey",
+    hex: "#323449",
+  },
+  white: {
+    name: "Lighthouse White",
+    hex: "#ebfafa	",
+  },
+  cyan: {
+    name: "Watery Tomb Blue",
+    hex: "#04d1f9",
+  },
+  green: {
+    name: "Great Old One Green",
+    hex: "#37f499",
+  },
+  orange: {
+    name: "Dreaming Orange",
+    hex: "#f7c67f",
+  },
+  pink: {
+    name: "Pustule Pink",
+    hex: "#f265b5",
+  },
+  purple: {
+    name: "Lovecraft Purple",
+    hex: "#a48cf2",
+  },
+  purpleDarker: {
+    name: "The Old One Purple",
+    hex: "#7081d0	",
+  },
+  red: {
+    name: "R'lyeh' Red",
+    hex: "#f16c75",
+  },
+  yellow: {
+    name: "Gold of Yuggoth",
+    hex: "#f1fc79",
   },
 };
 
 export const Fonts = Platform.select({
   ios: {
     /** iOS `UIFontDescriptorSystemDesignDefault` */
-    sans: 'system-ui',
+    sans: "system-ui",
     /** iOS `UIFontDescriptorSystemDesignSerif` */
-    serif: 'ui-serif',
+    serif: "ui-serif",
     /** iOS `UIFontDescriptorSystemDesignRounded` */
-    rounded: 'ui-rounded',
+    rounded: "ui-rounded",
     /** iOS `UIFontDescriptorSystemDesignMonospaced` */
-    mono: 'ui-monospace',
+    mono: "ui-monospace",
   },
   default: {
-    sans: 'normal',
-    serif: 'serif',
-    rounded: 'normal',
-    mono: 'monospace',
+    sans: "normal",
+    serif: "serif",
+    rounded: "normal",
+    mono: "monospace",
   },
   web: {
     sans: "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif",
     serif: "Georgia, 'Times New Roman', serif",
-    rounded: "'SF Pro Rounded', 'Hiragino Maru Gothic ProN', Meiryo, 'MS PGothic', sans-serif",
+    rounded:
+      "'SF Pro Rounded', 'Hiragino Maru Gothic ProN', Meiryo, 'MS PGothic', sans-serif",
     mono: "SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
   },
 });
