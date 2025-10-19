69216e48ef5606d3fbe6fa0c4a5e4306dac84952
lukas0173 <vuwin24680@gmail.com>
2025-10-19 15:39:54 +0700

    [Project] Re-organized the file structure and deleted redundant files. Added the system logs fetching API and call it in the root component just for testing

---
+++ mobile/src/api/system_logs.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const apiURL = `${process.env.EXPO_PUBLIC_API_URL}/collections/system_logs/records`;
+const abortController = new AbortController();
+
+export const fetch_system_logs = async (device_id: string) => {
+  try {
+    const systemLogsURL = `${apiURL}?filter=(device_id="${device_id}"`;
+    const response = await axios.get(systemLogsURL, {
+      signal: abortController.signal,
+    });
+
+    if (response.status === 200) {
+      console.log("[API] Successfully fetch system logs");
+      return response.data;
+    }
+  } catch (error) {
+    if (abortController.signal.aborted) {
+      console.log("[API] System logs fetching cancelled");
+    } else {
+      console.log("[API] System logs fetching error");
+    }
+    throw error;
+  }
+};
--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,24 +1,39 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/src/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { fetch_system_logs } from "@api/system_logs";
+import { useEffect, useState } from "react";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const [systemLogs, setSystemLogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const loadLogs = async () => {
+      try {
+        setIsLoading(true);
+        const returnedSystemLogs = await fetch_system_logs("tes");
+        setSystemLogs(returnedSystemLogs);
+      } catch (error: any) {
+        setError(error);
+        console.log(`[ERROR] ${error}`);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    loadLogs();
+  }, []);
+
+  console.log("[DEBUG] This is the system logs");
+  console.log(`[DEBUG] Is loading state: ${isLoading}`);
+  console.log(systemLogs);
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <SafeAreaView>
+      <View>
+        <Text>This is a test screen</Text>
+      </View>
+    </SafeAreaView>
   );
 }
